ebe060a413562e6b769b581e878e0818faef1e13
dani-vrd <dani@virtual-reality-dev.com>
2022-06-09 16:11:42 +0200

    init

---
+++ resources/js/example-vue-slot.vue
@@ -0,0 +1,24 @@
+<template>
+    <data-table>
+        <template #loading>
+            <!---->
+        </template>
+        <template #empty>
+            <!---->
+        </template>
+        <template #header>
+            <!---->
+        </template>
+        <column>
+            <template #body="row">
+                <!---->
+            </template>
+        </column>
+    </data-table>
+</template>
+
+<script setup>
+// PRIME-VUE
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+</script>
